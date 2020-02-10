76230c43c4a3831ee965ef6096a4715274f15e05
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-02-10 19:03:19 -0300

    Classes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-alert('Teste de Bundle');
+class List{
+    constructor(){
+        this.data = []
+    } 
+     add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+    
+}
+
+class TodoList extends List{
+    constructor(){
+        super();
+    }
+  
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');   
+}
