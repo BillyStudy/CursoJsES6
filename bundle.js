ef6661d20f1cfc071506c3416d461272a9ce043e
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-02-10 19:19:22 -0300

    Operações em Arrays

--- bundle.js
+++ bundle.js
@@ -1,11 +1,16 @@
 "use strict";
 
-function Soma(x) {
-  var y = 2;
-
-  if (x > 5) {
-    console.log(x, y);
-  }
-}
-
-Soma(10);
+var arr = [1, 3, 5, 7, 11];
+var newArr = arr.map(function (item) {
+  return item * 10;
+});
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+var find = arr.find(function (item) {
+  return item === 11;
+});
+console.log(find);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-function Soma(x) {
-    let y = 2;
+const arr = [1,3,5, 7,11];
 
-    if (x > 5) {
-        console.log(x, y);
-    }
-    
-}
+const newArr = arr.map(function(item){
+    return item * 10;
+})
 
-Soma(10);
+const sum = arr.reduce(function(total, next){
+    return total + next;
+})
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0
+})
+
+const find = arr.find(function(item){
+    return item === 11
+})
+
+console.log(find);
